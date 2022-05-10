6909a2cd746a0330a40f459ad28d3bd6303c584b
js.lozano@globant.com <142308Jslp>
2022-05-09 19:56:28 -0500

    Transactions class completed

--- src/main/java/com/automation/api/steps/Transactions.java
+++ src/main/java/com/automation/api/steps/Transactions.java
@@ -2,10 +2,19 @@ package com.automation.api.steps;
 
 import com.automation.api.pojo.Transaction;
 import io.restassured.http.ContentType;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.*;
 
 public class Transactions extends BaseStep {
+
+    private Logger log = Logger.getLogger(Transactions.class);
 
     /**
      * Constructor
@@ -47,6 +56,19 @@ public class Transactions extends BaseStep {
     }
 
     /**
+     * POST method. Create a list of transactions
+     * @param transactions {@link Transaction}
+     */
+    public void createTransaction(List<Transaction> transactions){
+        Transaction transaction;
+        for (int i = 0; i < transactions.size(); i++){
+            transaction = transactions.get(i);
+            response = given().contentType(ContentType.JSON).body(transaction)
+                    .when().post(endpoint);
+        }
+    }
+
+    /**
      * UPDATE method. Update account number in transaction
      * @param accountNumber long
      */
@@ -56,5 +78,42 @@ public class Transactions extends BaseStep {
 
         response = given().contentType(ContentType.JSON).body(transaction)
                 .when().put(endpoint + id);
+    }
+
+    /**
+     * Show list of transactions after GET request
+     */
+    public void showListOfTransactions(){
+        List<Transaction> transactions = response.then().extract().response()
+                .jsonPath().getList("$", Transaction.class);
+        log.info(transactions);
+    }
+
+    /**
+     * Delete all transactions
+     */
+    public void deleteTransactions(){
+        List<Transaction> transactions = response.then().extract().response()
+                .jsonPath().getList("$", Transaction.class);
+        List<String> transactionsId = new ArrayList<>();
+        transactions.forEach(transaction-> transactionsId.add(transaction.getId()));
+        for (String id : transactionsId) {
+            deleteTransaction(id);
+        }
+    }
+
+    /**
+     * True if there are repeated emails, false otherwise.
+     * @return boolean
+     */
+    public boolean areDuplicateEmails(){
+        List<Transaction> transactions = response.then().extract().response()
+                .jsonPath().getList("$", Transaction.class);
+        List<String> emails = new ArrayList<>();
+        transactions.forEach(transaction-> emails.add(transaction.getEmail()));
+        Set<String> nonRepeatedEmails = new HashSet<>(emails);
+        nonRepeatedEmails.addAll(emails);
+
+        return !(emails.size() == nonRepeatedEmails.size());
     }
 }
