5cd9160e36378c5b4b2eb3ee702b1cb0e34c1a0d
js.lozano@globant.com <142308Jslp>
2022-05-10 17:13:17 -0500

    GetTest completed, needed a refactor in suite to group tests

---
+++ src/test/java/com/automation/api/tests/GetTest.java
@@ -0,0 +1,15 @@
+package com.automation.api.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GetTest extends BaseTest{
+
+    @Test
+    public void testGetRequestWithoutDuplicateEmails(){
+        transactions_steps.getTransactions();
+        Assert.assertEquals(transactions_steps.getStatusCode(), 200,
+                "Status code is not correct");
+        Assert.assertFalse(transactions_steps.areDuplicateEmails(), "There are duplicate emails");
+    }
+}
