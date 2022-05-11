2600755bace7c35edd61f0ba257909f999797d79
js.lozano@globant.com <142308Jslp>
2022-05-11 11:25:43 -0500

    Completed refactoring in all tests, add Listener class

---
+++ src/test/java/com/automation/api/listeners/Listeners.java
@@ -0,0 +1,45 @@
+package com.automation.api.listeners;
+
+import org.apache.log4j.Logger;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener {
+    Logger log = Logger.getLogger(Listeners.class);
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+        log.info("Starting test");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        log.info("Successful test");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        log.info("Test failed");
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+}
--- src/test/java/com/automation/api/tests/PutTest.java
+++ src/test/java/com/automation/api/tests/PutTest.java
@@ -11,20 +11,27 @@ public class PutTest extends BaseTest{
             dataProviderClass = Data.class)
     public void testUpdateAccountNumber(long randomAccountNumber){
         transactions_steps.getRequest();
+        log.info("Get request status code: " + transactions_steps.getStatusCode());
         Assert.assertEquals(transactions_steps.getStatusCode(), 200,
                 "Status code is not correct");
-        String idLastTransaction = transactions_steps.getLastId();
-        Assert.assertNotNull(idLastTransaction, "There are no transactions");
 
+        String idLastTransaction = transactions_steps.getLastId();
+        log.info("Id of transaction to update: " + idLastTransaction);
+        Assert.assertNotNull(idLastTransaction, "There are no transactions");
         transactions_steps.getRequest(idLastTransaction);
+        log.info("Get request by id status code: " + transactions_steps.getStatusCode());
         Assert.assertEquals(transactions_steps.getStatusCode(), 200,
                 "Status code is not correct");
+
         Transaction transactionToUpdate = transactions_steps.getObjectResponse();
+        log.info("Transaction to update: " + transactionToUpdate.toString());
+        log.info("Actual account number: " + transactionToUpdate.getAccountNumber());
         transactionToUpdate.setAccountNumber(randomAccountNumber);
+        log.info("New account number: " + randomAccountNumber);
         transactions_steps.updateRequest(idLastTransaction, transactionToUpdate);
+        log.info("Put request status code: " + transactions_steps.getStatusCode());
 
         Assert.assertEquals(transactions_steps.getObjectResponse().getAccountNumber(),
                 transactionToUpdate.getAccountNumber(), "Accounts number was not updated");
-
     }
 }
