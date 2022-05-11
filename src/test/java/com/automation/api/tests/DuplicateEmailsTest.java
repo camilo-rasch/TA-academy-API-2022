b9629cfcf500775fc37421e107e25dd5d80681ba
js.lozano@globant.com <142308Jslp>
2022-05-11 11:11:09 -0500

    Refactor to DuplicateEmailsTest with correct logs

--- src/test/java/com/automation/api/tests/DuplicateEmailsTest.java
+++ src/test/java/com/automation/api/tests/DuplicateEmailsTest.java
@@ -3,13 +3,15 @@ package com.automation.api.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GetTest extends BaseTest{
+public class DuplicateEmailsTest extends BaseTest{
 
     @Test(groups = {"WithoutDeleteTransactions"})
     public void testGetRequestWithoutDuplicateEmails(){
         transactions_steps.getRequest();
+        log.info("Get request status code: " + transactions_steps.getStatusCode());
         Assert.assertEquals(transactions_steps.getStatusCode(), 200,
                 "Status code is not correct");
+        log.info("There are duplicate emails: " + transactions_steps.areDuplicateEmails());
         Assert.assertFalse(transactions_steps.areDuplicateEmails(), "There are duplicate emails");
     }
 }
